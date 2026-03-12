ec5c832d6d8d9fa5210d24f6108aaba97738ff3c
Jacob <jacobdhartzell@gmail.com>
2026-03-12 13:14:46 -0400

    removed event listener (total now 15)

--- script.js
+++ script.js
@@ -719,15 +719,7 @@ clearLogBtn.addEventListener("click", function() {
 });
 
 
-window.addEventListener("load", function() {
-    gameTitle.style.transform = "scale(0.9)";
-    gameTitle.style.opacity = "0";
-    gameTitle.style.transition = "transform 0.8s ease, opacity 0.8s ease";
-    setTimeout(function() {
-        gameTitle.style.transform = "scale(1)";
-        gameTitle.style.opacity = "1";
-    }, 100);
-});
+
 
 
 //  INITIALIZE GAME
